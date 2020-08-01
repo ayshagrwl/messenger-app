cfa51ea8c9b8748747ac4eb7cb2a4c2022cdc8ee
Ayush Agrawal <ayushagrawal251@gmail.com>
2020-08-01 15:44:25 +0530

    Updates

---
+++ precache-manifest.c4403fd10de4c3ddb88fa0292bc2909b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62a5566b7545123e1eeedd4e64620962",
+    "url": "/ayshagrwl/messenger-app/index.html"
+  },
+  {
+    "revision": "e5756b76b473b7ad0cb2",
+    "url": "/ayshagrwl/messenger-app/static/css/main.67a33622.chunk.css"
+  },
+  {
+    "revision": "25119f5b918fe45da706",
+    "url": "/ayshagrwl/messenger-app/static/js/2.97f675a6.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/ayshagrwl/messenger-app/static/js/2.97f675a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5756b76b473b7ad0cb2",
+    "url": "/ayshagrwl/messenger-app/static/js/main.9c29aa6d.chunk.js"
+  },
+  {
+    "revision": "59eb831d2448ca1f785f",
+    "url": "/ayshagrwl/messenger-app/static/js/runtime-main.650288ab.js"
+  }
+]);
